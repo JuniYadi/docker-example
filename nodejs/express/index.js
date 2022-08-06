1b36297321c6e64115a828089f18b110c47fb2f3
JuniYadi <juniyadi94@gmail.com>
2022-08-06 21:40:49 +0700

    Update Nodejs Express

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.set('trust proxy');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({ header: req.headers, ip: req.ip, server: req.hostname });
+});
+
+app.get('/env', (req, res) => {
+    res.send({ env: process.env });
 });
 
 app.listen(3000, () => {
